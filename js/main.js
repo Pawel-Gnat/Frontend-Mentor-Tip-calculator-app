cd1a66b244dfdf70b27204084b0ea01e13b19b7f
Paweł <pawel_gnat@o2.pl>
2022-08-12 13:13:55 +0200

    wrote my process of JS code

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*
+1. If all inputs are empty or not selected disable reset button else set button to working state
+2. If custom or bill number input contains string or some kind of calc display error "Not a number" or if contains negative value display "Negative value" 
+3. If tip input was selected and you want to change it to custom than remove checked state from declared value input
+4. If people input contains string or some kind of calc display error "Not a number" or if contains negative value display "Negative value" or if contains "zero" value display "Can't be zero" 
+5. Calculate tip amount by declaring custom value and divide by number of people or by calculating the percent of bill
+6. Calculate total value by adding calculated tip amount and divided bill value by number of people
+*/
