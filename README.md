11762249cd9c23a41acc8f2b227f1b2144fcbccc
Paweł <pawel_gnat@o2.pl>
2022-08-15 21:37:08 +0200

    finished task

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-In development
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Live Site URL: [Frontend Mentor Tip calculator app](https://pawel-gnat.github.io/Frontend-Mentor-Tip-calculator-app/)
+
+## My process
+
+Java Script:
+
+1. If all inputs are empty or not selected disable reset button else set button to working state
+2. If custom or bill number input contains string or some kind of calc display error "Not a number" or if contains negative value display "Negative value"
+3. If tip input was selected and you want to change it to custom than remove checked state from declared value input
+4. If custom input contains some value and you want to change it to declated tip value input, it resets to custom state again
+5. If people input contains string or some kind of calc display error "Not a number" or if contains negative value display "Negative value" or if contains "zero" value display "Can't be zero"
+6. Calculate tip amount by declaring custom value and divide by number of people or by calculating the percent of bill
+7. Calculate total value by adding calculated tip amount and divided bill value by number of people
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Grid
+- Mobile-first workflow
+- JavaScript
+
+### What I learned
+
+This app took me 21,5 hours of coding. I practiced my JS skills. I added error handling with text messages near inputs.
+
+Done is better than perfect, but I still find few errors here. My Java Script code could be written better, anyway I tried my best at this moment. I hope I will refacture it later.
+
+## Author
+
+- Frontend Mentor - [@Pawel-Gnat](https://www.frontendmentor.io/profile/Pawel-Gnat)
--- js/main.js
+++ js/main.js
@@ -8,130 +8,40 @@
 7. Calculate total value by adding calculated tip amount and divided bill value by number of people
 */
 
-const customTipInput = document.querySelector('.select_box__select--input')
-const customTipTextError = document.querySelector('.select_box__select--custom_error')
 const tipRadioButtonValue = document.querySelectorAll('.select_box__select--btn')
 const tipValueInputs = document.querySelectorAll('.select_box__select--btn')
-const billInput = document.querySelector('#billInput')
-const billTextError = document.querySelector('.select_box__bill_error')
-const peopleInput = document.querySelector('#peopleInput')
-const peopleTextError = document.querySelector('.select_box__people_error')
 const result = document.querySelectorAll('.text_box__result')
+const resetButton = document.querySelector('.outcome_box__reset_btn')
+const inputConfig = [
+	{
+		input: (billInput = document.querySelector('#billInput')),
+		error: (billTextError = document.querySelector('.select_box__bill_error')),
+	},
+	{
+		input: (customTipInput = document.querySelector('.select_box__select--input')),
+		error: (customTipTextError = document.querySelector('.select_box__select--custom_error')),
+		canBeZero: true,
+	},
+	{
+		input: (peopleInput = document.querySelector('#peopleInput')),
+		error: (peopleTextError = document.querySelector('.select_box__people_error')),
+	},
+]
+let tipValue
 
-customTipInput.addEventListener('click', e => {
-	tipValueInputs.forEach(input => {
-		input.checked = false
-	})
-})
-
-for (let i = 0; i < tipValueInputs.length; i++) {
-	tipValueInputs[i].addEventListener('click', e => {
-		customTipInput.value = ''
-	})
+function disableResetButton() {
+	if (billInput.value == '' && peopleInput.value == '') {
+		resetButton.preventDefault
+		resetButton.classList.add('disabled')
+		resetButton.disabled = true
+	} else {
+		resetButton.classList.remove('disabled')
+		resetButton.disabled = false
+	}
 }
 
-// billInput.addEventListener('change', e => {
-// 	text = billInput.value
-
-// 	if (isNaN(parseFloat(text)) == true) {
-// 		billInput.classList.add('input_error')
-// 		billTextError.classList.add('error')
-// 		billTextError.textContent = 'Not a number'
-// 	} else if (parseFloat(text) === 0) {
-// 		billInput.classList.add('input_error')
-// 		billTextError.classList.add('error')
-// 		billTextError.textContent = "Can't be zero"
-// 	} else if (parseFloat(text) < 0) {
-// 		billInput.classList.add('input_error')
-// 		billTextError.classList.add('error')
-// 		billTextError.textContent = 'Negative value'
-// 	} else {
-// 		calculateTip()
-// 		calculateTotal()
-// 		billInput.classList.remove('input_error')
-// 		billTextError.classList.remove('error')
-// 		billTextError.textContent = ''
-// 	}
-// })
-
-// customTipInput.addEventListener('change', e => {
-// 	text = customTipInput.value
-
-// 	if (isNaN(parseFloat(text)) == true) {
-// 		customTipInput.classList.add('input_error')
-// 		customTipTextError.classList.add('error')
-// 		customTipTextError.textContent = 'Not a number'
-// 	} else if (parseFloat(text) < 0) {
-// 		customTipInput.classList.add('input_error')
-// 		customTipTextError.classList.add('error')
-// 		customTipTextError.textContent = 'Negative value'
-// 	} else {
-// 		calculateTip()
-// 		calculateTotal()
-// 		customTipInput.classList.remove('input_error')
-// 		customTipTextError.classList.remove('error')
-// 		customTipTextError.textContent = ''
-// 	}
-// })
-
-// peopleInput.addEventListener('change', e => {
-// 	text = peopleInput.value
-
-// 	if (isNaN(parseFloat(text)) == true) {
-// 		peopleInput.classList.add('input_error')
-// 		peopleTextError.classList.add('error')
-// 		peopleTextError.textContent = 'Not a number'
-// 	} else if (parseFloat(text) === 0) {
-// 		peopleInput.classList.add('input_error')
-// 		peopleTextError.classList.add('error')
-// 		peopleTextError.textContent = "Can't be zero"
-// 	} else if (parseFloat(text) < 0) {
-// 		peopleInput.classList.add('input_error')
-// 		peopleTextError.classList.add('error')
-// 		peopleTextError.textContent = 'Negative value'
-// 	} else {
-// 		calculateTip()
-// 		calculateTotal()
-// 		peopleInput.classList.remove('input_error')
-// 		peopleTextError.classList.remove('error')
-// 		peopleTextError.textContent = ''
-// 	}
-// })
-
-// inputArr = [billInput, customTipInput, peopleInput]
-// inputErrorArr = [billTextError, customTipTextError, peopleTextError]
-
-// inputArr.forEach((input, text) => {
-// 	input.addEventListener('change', e => {
-// 		if (isNaN(parseFloat(input.value)) == true) {
-// 			input.classList.add('input_error')
-// 			text.classList.add('error')
-// 			text.textContent = 'Not a number'
-// 		} else if (parseFloat(input.value) === 0) {
-// 			input.classList.add('input_error')
-// 			text.classList.add('error')
-// 			text.textContent = "Can't be zero"
-// 		} else if (parseFloat(input.value) < 0) {
-// 			input.classList.add('input_error')
-// 			text.classList.add('error')
-// 			text.textContent = 'Negative value'
-// 		} else {
-// 			calculateTip()
-// 			calculateTotal()
-// 			input.classList.remove('input_error')
-// 			text.classList.remove('error')
-// 			text.textContent = ''
-// 		}
-// 	})
-// })
-
-const inputConfig = [
-	{ input: billInput, error: billTextError },
-	{ input: customTipInput, error: customTipTextError, canBeZero: true },
-	{ input: peopleInput, error: peopleTextError },
-]
-
 function getErrorMessage(value, canBeZero) {
+	// function displays error message while input contains inappropriate value
 	const parsedValue = parseFloat(value)
 	if (isNaN(parsedValue)) {
 		return 'Not a number'
@@ -144,6 +54,38 @@ function getErrorMessage(value, canBeZero) {
 	}
 }
 
+function calculateTotal() {
+	if (!customTipInput.value == '') {
+		result[0].textContent = '$' + (parseInt(customTipInput.value) / parseInt(peopleInput.value)).toFixed(2)
+	} else {
+		result[0].textContent = '$' + ((parseInt(billInput.value) * tipValue) / parseInt(peopleInput.value)).toFixed(2)
+	}
+
+	result[1].textContent =
+		'$' +
+		(
+			parseInt(billInput.value) / parseInt(peopleInput.value) +
+			parseInt(result[0].textContent.replace(/\D/g, '')) / 100
+		).toFixed(2)
+}
+
+customTipInput.addEventListener('click', e => {
+	// function unchecks my %tip inputs if they were checked
+	tipValueInputs.forEach(input => {
+		input.checked = false
+	})
+})
+
+for (let i = 0; i < tipValueInputs.length; i++) {
+	// function set my custom tip input to default (if his value was declared), after clicking whichever of my other %tip inputs
+	tipValueInputs[i].addEventListener('click', e => {
+		customTipInput.value = ''
+		tipValue = parseInt(tipRadioButtonValue[i].value) / 100 // variable tipValue is needed to calculate my tip amount for each person
+		calculateTotal()
+		disableResetButton()
+	})
+}
+
 inputConfig.forEach(({ input, error, canBeZero }) => {
 	input.addEventListener('change', e => {
 		const errorMsg = getErrorMessage(e.target.value, canBeZero)
@@ -153,62 +95,26 @@ inputConfig.forEach(({ input, error, canBeZero }) => {
 			input.classList.add('input_error')
 			error.classList.add('error')
 		} else {
-			calculateTip()
 			calculateTotal()
+			disableResetButton()
 			input.classList.remove('input_error')
 			error.classList.remove('error')
 		}
 	})
 })
 
-function calculateTip() {
-	let tipValue
-
-	for (let i = 0; i < tipRadioButtonValue.length; i++) {
-		if (tipRadioButtonValue[i].checked) {
-			tipValue = parseInt(tipRadioButtonValue[i].value) / 100
-		}
-		// else if (tipValue.value > 0) {
-		// 	result[0].textContent =
-		// 		'$' + ((parseInt(billInput.value) * parseInt(tipValue.value)) / parseInt(peopleInput.value)).toFixed(2)
-		// }
-
-		console.log(tipValue)
-	}
-
-	if (customTipInput.value > 0) {
-		result[0].textContent = '$' + (parseInt(customTipInput.value) / parseInt(peopleInput.value)).toFixed(2)
-	}
-	// else if (tipValue.value > 0) {
-	// 	result[0].textContent =
-	// 		'$' + ((parseInt(billInput.value) * parseInt(tipValue.value)) / parseInt(peopleInput.value)).toFixed(2)
-	// }
+function clearContent() {
+	inputConfig.forEach(input => {
+		input[0].value = ''
+	})
+	result[0].textContent = '$0.00'
+	result[1].textContent = '$0.00'
 }
 
-function calculateTotal() {
-	if (billInput.value == '' || peopleInput.value == '') {
-		result[0].textContent = '$0.00'
-		result[1].textContent = '$0.00'
-	} else {
-		result[1].textContent =
-			'$' +
-			(
-				parseInt(billInput.value) / parseInt(peopleInput.value) +
-				parseInt(result[0].textContent.replace(/\D/g, '')) / 100
-			).toFixed(2)
-	}
-}
+resetButton.addEventListener('click', e => {
+	clearContent()
+	resetButton.classList.add('disabled')
+	resetButton.disabled = true
+})
 
-/*
-else if (tipRadioButtonValue.checked) {
-		let tipValue
-
-		for (let i = 0; i < tipRadioButtonValue.length; i++) {
-			if (tipRadioButtonValue[i].checked) {
-				tipValue = parseInt(tipRadioButtonValue[i].value) / 100
-			}
-		}
-
-		result[0].textContent = '$' + ((parseInt(billInput.value) * tipValue.value) / peopleInput.value).toFixed(2)
-		result[1].textContent = '$' + (result[0].textContent + parseInt(billInput.value) / peopleInput.value).toFixed(2)
-*/
+window.onload = disableResetButton()
